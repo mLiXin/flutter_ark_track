356702de3c88a4ce703bdeeae307371d13b7e794
mLiXin <61540975@qq.com>
2021-04-16 16:40:46 +0800

    feat：架子

---
+++ lib/data/static/page_data.dart
@@ -0,0 +1,1 @@
+const List FUND_LIST = ["ARKK","ARKQ","ARKW","ARKG","ARKF","ARKX"];
--- lib/module/home_page.dart
+++ lib/module/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_builder/responsive_builder.dart';
+import 'package:flutter_ark_track/data/static/page_data.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -13,109 +13,113 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return ResponsiveBuilder(
-      builder: (context,sizeIndexformation) {
-        if (sizeIndexformation.isTablet) {
-          return Center(child: Text("you are web"),);
-        } else {
-          return DefaultTabController(
-            length: 3,
-            child: Scaffold(
-              appBar: AppBar(
-                title: Text("Track Demo"),
-                centerTitle: true,
-                bottom: TabBar(
-                  tabs: [
-                    Tab(text: "ARKK",),
-                    Tab(text: "ARKG",),
-                    Tab(text: "ARKX",),
-                  ],
-                ),
-              ),
-              body: TabBarView(
-                children: [
-                  Center(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: DataTable(
-                        sortColumnIndex: 1,
-                        sortAscending: false,
-                        columns: [
-                          DataColumn(label: Text("Name"), tooltip: "姓名"),
-                          DataColumn(label: Text("Year")),
-                          DataColumn(label: Text("Month")),
-                          DataColumn(label: Text("Day")),
-                          DataColumn(label: Text("Gender")),
-                          DataColumn(label: Text("Age")),
-                          DataColumn(label: Text("Address")),
-                        ],
-                        rows: [
-                          DataRow(
-                              selected: selectedIndex == 0,
-                              onSelectChanged: (selected) {
-                                if (selected) {
-                                  setState(() {
-                                    selectedIndex = 0;
-                                  });
-                                }
-                              },
-                              cells: [
-                                DataCell(Text("Dash")),
-                                DataCell(Text("2018")),
-                                DataCell(Text("12")),
-                                DataCell(Text("09")),
-                                DataCell(Text("Male")),
-                                DataCell(Text("18")),
-                                DataCell(Text("jfiodnsaonfdsa"))
-                              ]),
-                          DataRow(
-                              selected: selectedIndex == 1,
-                              onSelectChanged: (selected) {
-                                if (selected) {
-                                  setState(() {
-                                    selectedIndex = 1;
-                                  });
-                                }
-                              },
-                              cells: [
-                                DataCell(Text("Gopher")),
-                                DataCell(Text("2009")),
-                                DataCell(Text("12")),
-                                DataCell(Text("09")),
-                                DataCell(Text("Male")),
-                                DataCell(Text("18")),
-                                DataCell(Text("jfiodnsaonfdsa"))
-                              ]),
-                          DataRow(
-                              selected: selectedIndex == 2,
-                              onSelectChanged: (selected) {
-                                if (selected) {
-                                  setState(() {
-                                    selectedIndex = 2;
-                                  });
-                                }
-                              },
-                              cells: [
-                                DataCell(Text("Gopher")),
-                                DataCell(Text("2009")),
-                                DataCell(Text("12")),
-                                DataCell(Text("09")),
-                                DataCell(Text("Male")),
-                                DataCell(Text("18")),
-                                DataCell(Text("jfiodnsaonfdsa"))
-                              ]),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Center(child: Text("ARKG")),
-                  Center(child: Text("ARKX"))
-                ],
-              ),
-            ),
-          );
-        }
-      } ,
+    return DefaultTabController(
+      length: FUND_LIST.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Track Demo"),
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: _generateTabs(),
+          ),
+        ),
+        body: TabBarView(
+          children: _generateTabBarViews(),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _generateTabs() {
+    List<Widget> result = [];
+    for(String item in FUND_LIST) {
+      result.add(Tab(text: item,));
+    }
+    return result;
+  }
+
+  List<Widget> _generateTabBarViews() {
+    List<Widget> result = [];
+    for(String item in FUND_LIST) {
+      result.add(_getContent(item));
+    }
+    return result;
+  }
+
+  Widget _getContent(String fundName) {
+    return Center(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: DataTable(
+          sortColumnIndex: 1,
+          sortAscending: false,
+          columns: [
+            DataColumn(label: Text("Name"), tooltip: "姓名"),
+            DataColumn(label: Text("Year")),
+            DataColumn(label: Text("Month")),
+            DataColumn(label: Text("Day")),
+            DataColumn(label: Text("Gender")),
+            DataColumn(label: Text("Age")),
+            DataColumn(label: Text("Address")),
+          ],
+          rows: [
+            DataRow(
+                selected: selectedIndex == 0,
+                onSelectChanged: (selected) {
+                  if (selected) {
+                    setState(() {
+                      selectedIndex = 0;
+                    });
+                  }
+                },
+                cells: [
+                  DataCell(Text(fundName)),
+                  DataCell(Text("2018")),
+                  DataCell(Text("12")),
+                  DataCell(Text("09")),
+                  DataCell(Text("Male")),
+                  DataCell(Text("18")),
+                  DataCell(Text("jfiodnsaonfdsa"))
+                ]),
+            DataRow(
+                selected: selectedIndex == 1,
+                onSelectChanged: (selected) {
+                  if (selected) {
+                    setState(() {
+                      selectedIndex = 1;
+                    });
+                  }
+                },
+                cells: [
+                  DataCell(Text(fundName)),
+                  DataCell(Text("2009")),
+                  DataCell(Text("12")),
+                  DataCell(Text("09")),
+                  DataCell(Text("Male")),
+                  DataCell(Text("18")),
+                  DataCell(Text("jfiodnsaonfdsa"))
+                ]),
+            DataRow(
+                selected: selectedIndex == 2,
+                onSelectChanged: (selected) {
+                  if (selected) {
+                    setState(() {
+                      selectedIndex = 2;
+                    });
+                  }
+                },
+                cells: [
+                  DataCell(Text(fundName)),
+                  DataCell(Text("2009")),
+                  DataCell(Text("12")),
+                  DataCell(Text("09")),
+                  DataCell(Text("Male")),
+                  DataCell(Text("18")),
+                  DataCell(Text("jfiodnsaonfdsa"))
+                ]),
+          ],
+        ),
+      ),
     );
   }
 }
